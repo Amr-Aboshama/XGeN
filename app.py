8d20148c11377a098e2fb9bf71e85928e177f0e0
Ayman Azzam <aymanazzam63@gmail.com>
2021-07-10 22:56:50 +0200

    fix bugs and update preprocessor

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, request
 import os
 import uuid
 
+from Preprocessor.preprocessor import Preprocessor
+
+from InfoExtract.TopicExtractor import TopicExtractor
+
 from QAGen.Loader import Loader
 from QAGen.QGen import QGen
 from QAGen.mcq.MCQGen import MCQGen
@@ -9,9 +13,10 @@ from QAGen.boolean.BooleanGen import BoolGen
 from QAGen.tf.TFGen import TFGen
 from QAGen.shortq.ShortGen import ShortGen
 from QAGen.longq.LongGen import LongGen
-from InfoExtract.TopicExtractor import TopicExtractor
 from QAGen.anspred.AnswerPredictor import AnswerPredictor
+
 from Ranker.Ranker import Ranker
+
 
 loader = Loader()
 print("Done Loader")
@@ -24,19 +29,26 @@ mcqGen = MCQGen(qgen)
 shortGen = ShortGen(qgen)
 longGen = LongGen(qgen)
 ansPredict = AnswerPredictor(loader)
+
 topicExtract = TopicExtractor(loader)
 
 
 app = Flask(__name__)
 
 
-@app.route("/api/upload/PDF", methods['POST'])
+@app.route("/api/upload/PDF", methods=['POST'])
 def uploadPDF():
     file = request.files.get('pdf')
     cur_uuid = uuid.uuid1()
     topics = []
 
     # TODO : Handle Converting PDF to Text
+    preprocessor = Preprocessor()
+
+    for page in preprocessor.page_by_page('inputs/modeling.pdf'):
+        print(page)
+        print()
+    
 
     # TODO : Call the function to preprocess the text
     
@@ -50,7 +62,7 @@ def uploadPDF():
     }
 
 
-@app.route("/api/upload/text", methods['POST'])
+@app.route("/api/upload/text", methods=['POST'])
 def uploadText():
     text = request.form.get('text')
     cur_uuid = uuid.uuid1()
@@ -67,7 +79,7 @@ def uploadText():
         "topics": topics,
     }
 
-@app.route("/api/examSpecifications", methods['POST'])
+@app.route("/api/examSpecifications", methods=['POST'])
 def examSpecifications():
     cur_uuid = request.form.get('uuid')
     selected_topics = request.form.getlist('topics')
