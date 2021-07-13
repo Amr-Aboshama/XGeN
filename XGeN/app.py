865ba6b861a691d3f9ba9ae19d10e7cb45107e17
Ayman Azzam <aymanazzam63@gmail.com>
2021-07-13 20:35:01 +0200

    Higher priority for the topics that the user added it

--- XGeN/app.py
+++ XGeN/app.py
@@ -213,7 +213,7 @@ def examSpecifications():
             # Add the new topics to the phrases
             for keyword in selected_topics:
                 if keyword not in topics and phrase.lower().find(keyword) != -1:
-                    phrases[phrase].append(keyword)
+                    phrases[phrase].insert(0,keyword)
             
     # Filter The paragraphs based on the selected topics
     filtered_phrases = rank_phrases(selected_topics, phrases)
