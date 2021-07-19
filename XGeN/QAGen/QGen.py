8b9375cc27b89d5a39a213b3f2c8a38ec08468f1
Amr-Aboshama <31421825+Amr-Aboshama@users.noreply.github.com>
2021-07-19 20:38:16 +0200

    removing unnecessary variables

--- XGeN/QAGen/QGen.py
+++ XGeN/QAGen/QGen.py
@@ -4,9 +4,6 @@ class QGen:
     
     def __init__(self, loader):
         
-        self.nlp = loader.nlp
-        self.s2v = loader.s2v
-        self.fdist = loader.fdist
         self.normalized_levenshtein = loader.normalized_levenshtein
         self.rand = loader.rand
         
