a30f4c9da62c41ec98b16681779193bf3784b0f3
Amr-Aboshama <31421825+Amr-Aboshama@users.noreply.github.com>
2021-07-24 19:52:21 +0200

    Update encoding.py

--- XGeN/QAGen/encoding/encoding.py
+++ XGeN/QAGen/encoding/encoding.py
@@ -10,12 +10,12 @@ def greedy_decoding (inp_ids,attn_mask,model,tokenizer):
 
 def beam_search_decoding (inp_ids,attn_mask,model,tokenizer):
   beam_output = model.generate(input_ids=inp_ids,
-                                 attention_mask=attn_mask,
-                                 max_length=256,
-                               num_beams=10,
-                               num_return_sequences=3,
-                               no_repeat_ngram_size=2,
-                               early_stopping=True
+                                attention_mask=attn_mask,
+                                max_length=256,
+                                num_beams=10,
+                                num_return_sequences=3,
+                                no_repeat_ngram_size=2,
+                                early_stopping=True
                                )
   Questions = [tokenizer.decode(out, skip_special_tokens=True, clean_up_tokenization_spaces=True) for out in
                beam_output]
@@ -24,12 +24,12 @@ def beam_search_decoding (inp_ids,attn_mask,model,tokenizer):
 
 def topkp_decoding (inp_ids,attn_mask,model,tokenizer):
   topkp_output = model.generate(input_ids=inp_ids,
-                                 attention_mask=attn_mask,
-                                 max_length=256,
-                               do_sample=True,
-                               top_k=40,
-                               top_p=0.80,
-                               num_return_sequences=3,
+                                attention_mask=attn_mask,
+                                max_length=256,
+                                do_sample=True,
+                                top_k=40,
+                                top_p=0.80,
+                                num_return_sequences=3,
                                 no_repeat_ngram_size=2,
                                 early_stopping=True
                                )
