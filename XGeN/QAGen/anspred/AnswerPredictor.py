6047dd2931b0f0b1d6640c80e3930f7a534d7651
Amr-Aboshama <31421825+Amr-Aboshama@users.noreply.github.com>
2021-07-25 07:41:31 +0200

    moving answerpredictor

--- XGeN/QAGen/anspred/AnswerPredictor.py
+++ XGeN/QAGen/anspred/AnswerPredictor.py
@@ -1,7 +1,3 @@
-#nltk.download('brown')
-#nltk.download('stopwords')
-#nltk.download('popular')
-
 
 class AnswerPredictor:
           
@@ -10,19 +6,14 @@ class AnswerPredictor:
         self.device = loader.device
         self.model = loader.ap_model
 
-    def predict_answer(self,payload):
-        inp = {
-            "input_text": payload.get("input_text"),
-            "input_question" : payload.get("input_question")
-        }
 
-        context = inp["input_text"]
-        question = inp["input_question"]
-        input = "question: %s <s> context: %s </s>" % (question,context)
+    def predict_answer(self,payload):
+        
+        input = "question: %s <s> context: %s </s>" % (payload["question"], payload["context"])
 
         encoding = self.tokenizer.encode_plus(input, return_tensors="pt")
         input_ids, attention_masks = encoding["input_ids"].to(self.device), encoding["attention_mask"].to(self.device)
-        greedy_output = self.model.generate(input_ids=input_ids, attention_mask=attention_masks, max_length=256)
+        greedy_output = self.qg_model.generate(input_ids=input_ids, attention_mask=attention_masks, max_length=256)
         Question =  self.tokenizer.decode(greedy_output[0], skip_special_tokens=True,clean_up_tokenization_spaces=True)
         output = Question.strip().capitalize()
 
